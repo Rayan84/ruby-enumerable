86f7b195d438ab0979619c5e38cb0d773849994c
Alexander <60612329+alexander16108@users.noreply.github.com>
2021-12-24 13:48:23 +0100

    Update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -16,3 +16,12 @@ class MyList
   end
 end
 
+list = MyEnumeralbe.new(1, 2, 3, 4)
+
+puts(list.all? { |e| e > 5 })
+puts(list.all? { |e| e > 5 })
+
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+
+puts(list.list.filter(&:even?))
